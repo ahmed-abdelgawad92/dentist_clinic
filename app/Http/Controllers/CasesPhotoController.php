f7f67254fa6dadcae25169d40acab0db2a7a2050
Ahmed Abdelgawad <ahmed.elkayaty92@gmail.com>
2018-07-10 23:44:01 +0200

    creates cases_photo table and its model&controller , also teeth table creation

---
+++ app/Http/Controllers/CasesPhotoController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\CasesPhoto;
+use Illuminate\Http\Request;
+
+class CasesPhotoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\CasesPhoto  $casesPhoto
+     * @return \Illuminate\Http\Response
+     */
+    public function show(CasesPhoto $casesPhoto)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\CasesPhoto  $casesPhoto
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(CasesPhoto $casesPhoto)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\CasesPhoto  $casesPhoto
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, CasesPhoto $casesPhoto)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\CasesPhoto  $casesPhoto
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(CasesPhoto $casesPhoto)
+    {
+        //
+    }
+}
